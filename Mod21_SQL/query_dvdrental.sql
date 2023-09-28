498d4feb447d9e610164f7f90fee5950e2be9e99
Fabricio Zillig <fabricio.zillig@gmail.com>
2023-09-28 16:42:48 -0300

    Finalizando Exercício 2 - Mod 21

---
+++ Mod21_SQL/query_dvdrental.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) AS "Contagem ticket_flights"
+FROM ticket_flights;
+
+SELECT COUNT(*) AS "Contagem aircrafts_data"
+FROM aircrafts_data;
+
+SELECT COUNT(*) AS "Contagem flights"
+FROM flights;
+
+SELECT COUNT(*) AS "Contagem airports_data"
+FROM airports_data;
+
+SELECT COUNT(*) AS "Contagem bookings"
+FROM bookings
