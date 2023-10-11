1eecd85714aa2394d06bef9bd599ff6faaa6c79a
Fabricio Zillig <fabricio.zillig@gmail.com>
2023-10-11 15:46:02 -0300

    Corrigindo problema no Exercicio do Mod 01 - SQL Extra

--- Extra_SQL/Mod01_exercicio_1.sql
+++ Extra_SQL/Mod01_exercicio_1.sql
@@ -1,5 +1,5 @@
 -- @block create table
-CREATE TABLE clientes (
+CREATE TABLE clientes2 (
   id BIGINT, 
   idade BIGINT, 
   sexo STRING, 
@@ -12,9 +12,15 @@ CREATE TABLE clientes (
 );
 
 -- @block insert data
-INSERT INTO clientes VALUES (768805383, 45, 'M', 3, 'ensino medio', 'blue', 12691.51, 1144.90, 42);
-INSERT INTO clientes VALUES (818770008, 49, 'F', 5, 'mestrado', 'blue', 8256.96, 1291.45, 33);
-INSERT INTO clientes VALUES (713982108, 51, 'M', 3, 'mestrado', 'blue', 3418.56, 1887.72, 20);
+INSERT INTO clientes2 VALUES (768805383, 45, 'M', 3, 'ensino medio', 'blue', 12691.51, 1144.90, 42);
+INSERT INTO clientes2 VALUES (818770008, 49, 'F', 5, 'mestrado', 'blue', 8256.96, 1291.45, 33);
+INSERT INTO clientes2 VALUES (713982108, 51, 'M', 3, 'mestrado', 'blue', 3418.56, 1887.72, 20);
 
--- @block
-SELECT * FROM clientes WHERE sexo = 'M' AND idade > 50;
+-- @block Query 1
+SELECT * FROM clientes2;
+
+-- @block Query 2
+SELECT id, idade, limite_credito FROM clientes2 WHERE sexo = 'M' ORDER BY idade DESC;
+
+-- @block Query 3
+SELECT sexo, AVG(idade) AS "media_idade_por_sexo" FROM clientes2 GROUP BY sexo;
